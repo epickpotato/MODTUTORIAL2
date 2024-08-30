5b522e7b67ef490a5d9107e1d7ba7c35772fc0e7
franc <francochavez705@gmail.com>
2024-08-29 21:02:33 -0300

    a

--- src/main/java/modtutorial/modtutorial/item/Moditems.java
+++ src/main/java/modtutorial/modtutorial/item/Moditems.java
@@ -1,6 +1,7 @@
 package modtutorial.modtutorial.item;
 
 import modtutorial.modtutorial.Modtutorial;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -12,7 +13,9 @@ public class Moditems {
             DeferredRegister.create(ForgeRegistries.ITEMS, Modtutorial.MODID);
 
     public static final RegistryObject<Item> POTATO=
-            ITEMS.register("potato", () -> new Item( new Item.Properties()));
+            ITEMS.register("potato", () -> new Item(
+                    new Item.Properties().food(new FoodProperties.Builder().nutrition(6).saturationMod(1.2f).build())));
+
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
